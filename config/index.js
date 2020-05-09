a490d9dcc08c1c79df77d5acc52b4a80c4942154
garcia98daniel <garcia98daniel@gmail.com>
2020-05-09 16:47:18 -0500

    carpeta de scripts agregadas

--- config/index.js
+++ config/index.js
@@ -7,7 +7,11 @@ const config = {
 	dbUser: process.env.DB_USER,
 	dbPassword: process.env.DB_PASSWORD,
 	dbHost: process.env.DB_HOST,
-	dbName: process.env.DB_NAME
+	dbName: process.env.DB_NAME,
+	default_admin_password:process.env.DEFAULT_ADMIN_PASSWORD,
+	default_user_password:process.env.DEFAULT_USER_PASSWORD,
+	public_api_key_token: process.env.PUBLIC_API_KEY_TOKEN,
+	admin_api_key_token:process.env.ADMIN_API_KEY_TOKEN
 }
 
 module.exports = { config };
